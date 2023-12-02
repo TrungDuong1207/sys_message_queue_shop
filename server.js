42f1f10ae7230a078bee2f11ef0199c951c36f87
Trung Duong <duongtrung120796@gmail.com>
2023-12-02 10:02:35 +0700

    rbmq notification exchange DLX

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const { consumerToQueue } = require('./src/services/consumerQueue.service');
+const { consumerToQueue, consumerToQueueNormal, consumerToQueueFail } = require('./src/services/consumerQueue.service');
 const queueName = 'test-topic';
 
-consumerToQueue(queueName).then(() => {
-    console.log(`Message consumer startes ${queueName}`)
+// consumerToQueue(queueName).then(() => {
+//     console.log(`Message consumer startes ${queueName}`)
+// }).catch(err => {
+//     console.error(`message error:`, err)
+// })
+
+consumerToQueueNormal().then(() => {
+    console.log(`Message consumer started`)
+}).catch(err => {
+    console.error(`message error:`, err)
+})
+
+consumerToQueueFail().then(() => {
+    console.log(`Message consumer fail started`)
 }).catch(err => {
     console.error(`message error:`, err)
 })
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -13,7 +13,52 @@ const messageService = {
         } catch (e) {
             console.error(`Error consumerToQueue`, e);
         }
-    }
+    },
+    //case processing
+    consumerToQueueNormal: async () => {
+        try {
+            const { channel, connection } = await connectToRabbitMQ()
+            const notiQueue = 'notificationQueueProcess';
+
+            const timeExpired = 5000;
+            setTimeout(() => {
+                channel.consume(notiQueue, msg => {
+                    console.log(`send notification successfully processes:`, msg.content.toString());
+                    channel.ack(msg)
+                })
+            }, timeExpired)
+
+        } catch (e) {
+            console.error(e)
+        }
+    },
+    //case fail
+    consumerToQueueFail: async () => {
+        try {
+            const { channel, connection } = await connectToRabbitMQ()
+            const notificationExchangeDLX = 'notificationExDLX';
+            const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX';
+            const notiQueueHandler = 'notificationQueueHotFix';
+
+            await channel.assertExchange(notificationExchangeDLX, 'direct', {
+                durable: true
+            });
+
+            const queueResult = await channel.assertQueue(notiQueueHandler, {
+                exclusive: false
+            });
+            await channel.bindQueue(queueResult.queue, notificationExchangeDLX, notificationRoutingKeyDLX);
+
+            await channel.consume(queueResult.queue, msgFailed => {
+                console.log(`this notification error, pls hot fix`, msgFailed.content.toString())
+            }, {
+                noAck: true
+            })
+        } catch (e) {
+            console.error(e);
+            throw e
+        }
+    },
 
 }
 
