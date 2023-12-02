a010a64e4985270fe2399ff3e7c1504753cad114
Trung Duong <duongtrung120796@gmail.com>
2023-12-02 10:25:01 +0700

    rbmq test lỗi logic

--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -19,14 +19,36 @@ const messageService = {
         try {
             const { channel, connection } = await connectToRabbitMQ()
             const notiQueue = 'notificationQueueProcess';
+            //1. TTL vd lỗi vượt thời gian sống
+            // const timeExpired = 5000;
+            // setTimeout(() => {
+            //     channel.consume(notiQueue, msg => {
+            //         console.log(`send notification successfully processes:`, msg.content.toString());
+            //         channel.ack(msg)
+            //     })
+            // }, timeExpired)
 
-            const timeExpired = 5000;
-            setTimeout(() => {
-                channel.consume(notiQueue, msg => {
+            //2. lỗi logic
+            channel.consume(notiQueue, msg => {
+                try {
+                    const numberTest = Math.random();
+                    console.log({ numberTest });
+                    if (numberTest < 0.8) {
+                        throw new Error('Send notification failes: HOT FIX')
+                    }
                     console.log(`send notification successfully processes:`, msg.content.toString());
                     channel.ack(msg)
-                })
-            }, timeExpired)
+                } catch (e) {
+                    // console.error(`Ssend notification error`, e);
+                    channel.nack(msg, false, false);
+                    /*
+                        nack: nagative acknowledgement
+                        tham số thứ 2: true là sẽ đẩy lại hàng đợi normal, còn false là đẩy xuống hàng đợi lỗi
+                        tham số thứ 3: có từ chối nhiều message hay không(false nghĩa là chỉ tin nhắn hiện tại sẽ bị từ chối mà thôi)
+                    */
+                }
+
+            })
 
         } catch (e) {
             console.error(e)
